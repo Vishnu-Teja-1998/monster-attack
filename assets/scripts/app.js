27fc0d9fa5902cdb8387724cf8c96776549cc96a
vishnu <chilamakurvishnu@gmail.com>
2020-08-28 11:49:29 +0530

    switch statement is replaced with if-else block for consice code

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -30,45 +30,54 @@ adjustHealthBars(choosenMaxHealth);
 function writeToLog(evt, val, monsterHealth, playerHealth) {
   //evt = event, val = value
   let logEntry;
-  if (evt === LOG_EVENT_PLAYER_ATTACK) {
-    logEntry = {
-      event: evt,
-      value: val,
-      target: "MONSTER",
-      finalMonsterHealth: monsterHealth,
-      finalPlayerHealth: playerHealth,
-    };
-  } else if (evt === LOG_EVENT_PLAYER_STRONG_ATTACK) {
-    logEntry = {
-      event: evt,
-      value: val,
-      target: "MONSTER",
-      finalMonsterHealth: monsterHealth,
-      finalPlayerHealth: playerHealth,
-    };
-  } else if (evt === LOG_EVENT_MONSTER_ATTACK) {
-    logEntry = {
-      event: evt,
-      value: val,
-      target: "PLAYER",
-      finalMonsterHealth: monsterHealth,
-      finalPlayerHealth: playerHealth,
-    };
-  } else if (evt === LOG_EVENT_PLAYER_HEAL) {
-    logEntry = {
-      event: evt,
-      value: val,
-      target: "PLAYER",
-      finalMonsterHealth: monsterHealth,
-      finalPlayerHealth: playerHealth,
-    };
-  } else if (evt === LOG_EVENT_GAME_OVER) {
-    logEntry = {
-      event: evt,
-      value: val,
-      finalMonsterHealth: monsterHealth,
-      finalPlayerHealth: playerHealth,
-    };
+
+  switch (evt) {
+    case LOG_EVENT_PLAYER_ATTACK:
+      logEntry = {
+        event: evt,
+        value: val,
+        target: "MONSTER",
+        finalMonsterHealth: monsterHealth,
+        finalPlayerHealth: playerHealth,
+      };
+      break;
+    case LOG_EVENT_PLAYER_STRONG_ATTACK:
+      logEntry = {
+        event: evt,
+        value: val,
+        target: "MONSTER",
+        finalMonsterHealth: monsterHealth,
+        finalPlayerHealth: playerHealth,
+      };
+      break;
+    case LOG_EVENT_MONSTER_ATTACK:
+      logEntry = {
+        event: evt,
+        value: val,
+        target: "PLAYER",
+        finalMonsterHealth: monsterHealth,
+        finalPlayerHealth: playerHealth,
+      };
+      break;
+    case LOG_EVENT_PLAYER_HEAL:
+      logEntry = {
+        event: evt,
+        value: val,
+        target: "PLAYER",
+        finalMonsterHealth: monsterHealth,
+        finalPlayerHealth: playerHealth,
+      };
+      break;
+    case LOG_EVENT_GAME_OVER:
+      logEntry = {
+        event: evt,
+        value: val,
+        finalMonsterHealth: monsterHealth,
+        finalPlayerHealth: playerHealth,
+      };
+      break;
+    default:
+      logentry = {};
   }
   battleLog.push(logEntry);
 }
